d71a7a438a3ef9404088569944d692ba033dfe19
Vinz2008 <vincentbidarddelanoe@gmail.com>
2024-06-04 23:17:00 +0200

    work on x86_64 and aarch64 dist docker

---
+++ docker_dev/x86_64/build-clang-llvm.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+# Try to keep the LLVM version here in sync with src/ci/scripts/install-clang.sh
+LLVM=llvmorg-17.0.6
+
+mkdir llvm-project
+cd llvm-project
+
+curl -L https://github.com/llvm/llvm-project/archive/$LLVM.tar.gz | \
+  tar xzf - --strip-components=1
+
+mkdir clang-build
+cd clang-build
+
+# For whatever reason the default set of include paths for clang is different
+# than that of gcc. As a result we need to manually include our sysroot's
+# include path, /rustroot/include, to clang's default include path.
+INC="/build/cpoint-root/include:/usr/include"
+
+# We need compiler-rt for the profile runtime (used later to PGO the LLVM build)
+# but sanitizers aren't currently building. Since we don't need those, just
+# disable them. BOLT is used for optimizing LLVM.
+    cmake ../llvm \
+      -DCMAKE_C_COMPILER=/build/cpoint-root/bin/gcc \
+      -DCMAKE_CXX_COMPILER=/build/cpoint-root/bin/g++ \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/build/cpoint-root \
+      -DCOMPILER_RT_BUILD_SANITIZERS=OFF \
+      -DCOMPILER_RT_BUILD_XRAY=OFF \
+      -DCOMPILER_RT_BUILD_MEMPROF=OFF \
+      -DLLVM_TARGETS_TO_BUILD=X86 \
+      -DLLVM_INCLUDE_BENCHMARKS=OFF \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_EXAMPLES=OFF \
+      -DLLVM_ENABLE_PROJECTS="clang;lld;compiler-rt;bolt" \
+      -DC_INCLUDE_DIRS="$INC"
+
+make -j$(nproc)
+make install
+
+cd ../..
+rm -rf llvm-project
