61bf8a5baefcc8f65b63aeafd1b9b637fd5aab92
Vinz2008 <vincentbidarddelanoe@gmail.com>
2024-06-29 22:58:03 +0200

    work on aarch64 docker_dev

--- docker_dev/aarch64/run-container.sh
+++ docker_dev/aarch64/run-container.sh
@@ -1,1 +1,9 @@
-../../shared_scripts/run-container.sh
+#!/bin/bash
+set -ex
+
+#DOCKER_IMAGE=$(docker images | tail -n +2 | head -n 1 | cut -d " " -f12)
+DOCKER_IMAGE=$(docker images | tail -n +2 | head -n 1 | awk -v FIELDWIDTHS="20 20 20 20 20" '{ print $3 }')
+docker run -d --name docker_dev --platform linux/arm64 $DOCKER_IMAGE bash
+docker exec -it docker_dev /bin/bash
+docker stop docker_dev
+docker rm docker_dev
--- shared_scripts/run-container.sh
+++ shared_scripts/run-container.sh
@@ -3,7 +3,7 @@ set -ex
 
 #DOCKER_IMAGE=$(docker images | tail -n +2 | head -n 1 | cut -d " " -f12)
 DOCKER_IMAGE=$(docker images | tail -n +2 | head -n 1 | awk -v FIELDWIDTHS="20 20 20 20 20" '{ print $3 }')
-docker run -d --name docker_dev --platform linux/arm64 $DOCKER_IMAGE bash
+docker run -d --name docker_dev $DOCKER_IMAGE bash
 docker exec -it docker_dev /bin/bash
 docker stop docker_dev
 docker rm docker_dev
